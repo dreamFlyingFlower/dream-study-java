e261087533d24cc1a59fa13d9a50d13fbaee1cf0
飞花梦影 <582822832@qq.com>
2021-06-25 09:31:13 +0000

    study

---
+++ dream-study-java-common/src/main/java/com/wy/jdks/JDK13.java
@@ -0,0 +1,17 @@
+package com.wy.jdks;
+
+/**
+ * JDK13新特性
+ * 
+ * <pre>
+ * 1.swtich-case的case中可以使用{}写复杂的逻辑
+ * 2.文本块的支持.类似Python中的"""内容""",可以在"""中进行换行操作,而且输出是原样输出
+ * </pre>
+ *
+ * @author 飞花梦影
+ * @date 2021-06-25 10:30:47
+ * @git {@link https://github.com/dreamFlyingFlower}
+ */
+public class JDK13 {
+
+}
---
+++ dream-study-java-common/src/main/java/com/wy/jdks/JDK9.java
@@ -0,0 +1,18 @@
+package com.wy.jdks;
+
+/**
+ * JDK9新特性
+ * 
+ * <pre>
+ * 1.接口中可定义私有方法,默认方法可调用私有方法.应该可以定义私有静态方法在pulib静态方法调用吧,MayBe
+ * 2.自动推断类型运算符<>在JDK9之前不支持匿名内部类,在 JAVA 9 中也对匿名内部类做了支持
+ * 3.try-with-resources可自动关闭资源,但必须写在try中,JDK9可以引用try代码块之外的变量自动关闭
+ * </pre>
+ *
+ * @author 飞花梦影
+ * @date 2021-06-25 10:01:46
+ * @git {@link https://github.com/dreamFlyingFlower}
+ */
+public class JDK9 {
+
+}
