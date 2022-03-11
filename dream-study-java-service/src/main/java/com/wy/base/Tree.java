7398b9066835a39aa3296851c3bc70b0a4b8e761
飞花梦影 <yang_wan@bjdv.com>
2022-03-11 18:02:33 +0800

    study something

--- dream-study-java-service/src/main/java/com/wy/base/Tree.java
+++ dream-study-java-service/src/main/java/com/wy/base/Tree.java
@@ -1,7 +1,12 @@
 package com.wy.base;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
+import com.wy.collection.ListTool;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,19 +26,19 @@ public class Tree<T, ID> extends AbstractModel {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 树形结果标识符,若该字段可能重复时,使用treeCode
+	 * 树形结果标识符,该字段不可重复
 	 */
 	private ID treeId;
+
+	/**
+	 * 树形结构上级标识符
+	 */
+	private ID treePid;
 
 	/**
 	 * 树形结构需要展示的名称
 	 */
 	private String treeName;
-
-	/**
-	 * 树形结构的Code
-	 */
-	private String treeCode;
 
 	/**
 	 * 树形结构中的扩展数据
@@ -49,4 +54,34 @@ public class Tree<T, ID> extends AbstractModel {
 	 * 下层数据列表
 	 */
 	private List<T> children;
+
+	/**
+	 * 将列表转成树形结构,但是
+	 * 
+	 * @param <T>
+	 * @param <ID>
+	 * @param datas
+	 * @return
+	 */
+	public static <T extends Tree<T, ID>, ID> List<Tree<T, ID>> transList2Tree(List<T> datas) {
+		Map<ID, Tree<T, ID>> map = new HashMap<>();
+		datas.forEach(t -> {
+			map.put(t.getTreeId(), t);
+		});
+		List<Tree<T, ID>> rets = new ArrayList<>();
+		datas.forEach(t -> {
+			Tree<T, ID> parentTree = map.get(t.getTreePid());
+			if (Objects.isNull(parentTree)) {
+				rets.add(parentTree);
+			} else {
+				List<T> children = parentTree.getChildren();
+				if (ListTool.isEmpty(children)) {
+					children = new ArrayList<T>();
+					parentTree.setChildren(children);
+				}
+				children.add(t);
+			}
+		});
+		return rets;
+	}
 }
