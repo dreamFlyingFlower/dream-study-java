efb0c5b8c874f5ebbc15fc03df3dc88f0aa5e91c
万杨 <yang_wan@bjdv.com>
2022-11-11 08:37:05 +0800

    study something

--- dream-study-java-web/src/main/java/com/wy/config/GlobalWebConfig.java
+++ dream-study-java-web/src/main/java/com/wy/config/GlobalWebConfig.java
@@ -1,58 +1,81 @@
-//package com.wy.config;
-//
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import com.wy.filters.SecondFilter;
-//import com.wy.interceptor.FirstInterceptor;
-//import com.wy.interceptor.RateLimitInterceptor;
-//import com.wy.servlet.listener.SecondListener;
-//
-///**
-// * Filter,Intercpetor,Listener等初始化,最好实现WebMvcConfigurer而不继承{@link WebMvcConfigurationSupport},有可能会改变json序列化方式
-// *
-// * @author 飞花梦影
-// * @date 2021-03-26 15:36:50
-// * @git {@link https://github.com/dreamFlyingFlower}
-// */
-////@Configuration
-//public class GlobalWebConfig implements WebMvcConfigurer {
-//
-//	/**
-//	 * 添加过滤器.若有多个过滤器,可以写多个类似方法
-//	 * 
-//	 * @return 过滤器注册对象
-//	 */
-//	@Bean
-//	public FilterRegistrationBean<?> secondFilterRegistration() {
-//		FilterRegistrationBean<SecondFilter> bean = new FilterRegistrationBean<>(new SecondFilter());
-//		bean.addUrlPatterns("/second");
-//		return bean;
-//	}
-//
-//	/**
-//	 * 添加监听器,可以添加多个
-//	 * 
-//	 * @return 监听器对象
-//	 */
-//	@Bean
-//	public ServletListenerRegistrationBean<SecondListener> getServletListenerRegistrationBean() {
-//		ServletListenerRegistrationBean<SecondListener> bean =
-//				new ServletListenerRegistrationBean<SecondListener>(new SecondListener());
-//		return bean;
-//	}
-//
-//	/**
-//	 * 添加拦截器,可以添加多个
-//	 */
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-////		registry.addInterceptor(new FirstInterceptor());
-//		registry.addInterceptor(new RateLimitInterceptor());
-//	}
-//}
+package com.wy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.wy.filters.SecondFilter;
+import com.wy.interceptor.FirstInterceptor;
+import com.wy.interceptor.RateLimitInterceptor;
+import com.wy.servlet.listener.SecondListener;
+
+/**
+ * Filter,Intercpetor,Listener等初始化,最好实现WebMvcConfigurer而不继承{@link WebMvcConfigurationSupport},有可能会改变json序列化方式
+ * 
+ * @author 飞花梦影
+ * @date 2021-03-26 15:36:50
+ * @git {@link https://github.com/dreamFlyingFlower}
+ */
+@Configuration
+public class GlobalWebConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private FirstInterceptor firstInterceptor;
+
+	/**
+	 * 添加过滤器.若有多个过滤器,可以写多个类似方法
+	 * 
+	 * @return 过滤器注册对象
+	 */
+	@Bean
+	public FilterRegistrationBean<?> secondFilterRegistration() {
+		FilterRegistrationBean<SecondFilter> bean = new FilterRegistrationBean<>(new SecondFilter());
+		bean.addUrlPatterns("/second");
+		return bean;
+	}
+
+	/**
+	 * 添加监听器,可以添加多个
+	 * 
+	 * @return 监听器对象
+	 */
+	@Bean
+	public ServletListenerRegistrationBean<SecondListener> getServletListenerRegistrationBean() {
+		ServletListenerRegistrationBean<SecondListener> bean =
+		        new ServletListenerRegistrationBean<SecondListener>(new SecondListener());
+		return bean;
+	}
+
+	/**
+	 * 添加拦截器,可以添加多个
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new RateLimitInterceptor());
+		// 直接将对象注入或spring注入
+		// registry.addInterceptor(new FirstInterceptor());
+		registry.addInterceptor(firstInterceptor)
+		        // 拦截的请求,只对单个拦截器有效,每个拦截都需要配置,不配置默认拦截所有请求
+		        .addPathPatterns("/**")
+		        // 不拦截的请求
+		        .excludePathPatterns("/bigscreen/template1/js/jquery.easyui.min.js", "/static/**",
+		                "/bigscreen/template1/js/*.js", "/bigscreen/tempate1/**", "/bigscreen/**",
+		                "/bigscreen/template1/js/**");
+		// 若interceptor没有加入到springboot上下文中,可以直接new一个,效果同上
+		// registry.addInterceptor(new FirstInterceptor());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 不拦截的资源请求同时要添加到资源映射中
+		registry.addResourceHandler("/bigscreen/**").addResourceLocations("classpath:/static/bigscreen/");
+		registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
--- dream-study-java-web/src/main/java/com/wy/filters/SecondFilter.java
+++ dream-study-java-web/src/main/java/com/wy/filters/SecondFilter.java
@@ -1,54 +1,54 @@
-//package com.wy.filters;
-//
-//import java.io.IOException;
-//
-//import javax.servlet.Filter;
-//import javax.servlet.FilterChain;
-//import javax.servlet.FilterConfig;
-//import javax.servlet.ServletException;
-//import javax.servlet.ServletRequest;
-//import javax.servlet.ServletResponse;
-//
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import com.wy.config.GlobalWebConfig;
-//
-///**
-// * servlet的拦截器,该方式需要当前类被注入到{@link FilterRegistrationBean}中,不需要添加{@link Configuration}
-// *
-// * @author ParadiseWY
-// * @date 2019-04-07 10:32:36
-// * @git {@link https://github.com/mygodness100}
-// */
-//public class SecondFilter implements Filter {
-//
-//	@Override
-//	public void destroy() {
-//
-//	}
-//
-//	@Override
-//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//			throws IOException, ServletException {
-//		chain.doFilter(request, response);
-//	}
-//
-//	@Override
-//	public void init(FilterConfig arg0) throws ServletException {
-//
-//	}
-//
-//	/**
-//	 * 该方法可以写在{@link GlobalWebConfig}中,也可以写这里
-//	 * 
-//	 * @return 过滤器对象
-//	 */
-//	@Bean
-//	public FilterRegistrationBean<SecondFilter> secondFilter() {
-//		FilterRegistrationBean<SecondFilter> bean = new FilterRegistrationBean<>(new SecondFilter());
-//		bean.addUrlPatterns("/secondFilter");
-//		return bean;
-//	}
-//}
+package com.wy.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.wy.config.GlobalWebConfig;
+
+/**
+ * servlet的拦截器,该方式需要当前类被注入到{@link FilterRegistrationBean}中,不需要添加{@link Configuration}
+ *
+ * @author ParadiseWY
+ * @date 2019-04-07 10:32:36
+ * @git {@link https://github.com/mygodness100}
+ */
+public class SecondFilter implements Filter {
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+	/**
+	 * 该方法可以写在{@link GlobalWebConfig}中,也可以写这里
+	 * 
+	 * @return 过滤器对象
+	 */
+	@Bean
+	public FilterRegistrationBean<SecondFilter> secondFilter() {
+		FilterRegistrationBean<SecondFilter> bean = new FilterRegistrationBean<>(new SecondFilter());
+		bean.addUrlPatterns("/secondFilter");
+		return bean;
+	}
+}
