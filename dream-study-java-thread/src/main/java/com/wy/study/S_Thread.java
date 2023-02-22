537621744bb5c54b0675e9b5169a5e57de379277
万杨 <yang_wan@bjdv.com>
2023-02-22 17:50:23 +0800

    study something

--- dream-study-java-thread/src/main/java/com/wy/study/S_Thread.java
+++ dream-study-java-thread/src/main/java/com/wy/study/S_Thread.java
@@ -14,6 +14,8 @@ import java.util.concurrent.TimeUnit;
  * 
  * 线程的打断,恢复:Interrupt()
  * 
+ * yield:会让当前线程从Running->Runnable,然后调度执行其他同优先级的线程,如果没有同优先级线程,可能不会有暂停效果
+ * 
  * @author 飞花梦影
  * @date 2019-05-08 20:39:35
  * @git {@link https://github.com/dreamFlyingFlower}
@@ -57,14 +59,16 @@ class TInterrupt implements Runnable {
 
 		while (true) {
 			// 线程中断标志,interrupt(),wait(),sleep(),join()都会造成线程中断
-			// 判断线程是否已经中断,即是否调用过interrupt(),调用过则返回true
+			// 判断线程是否有打断标记,即是否调用过interrupt(),调用过则返回true,该方法不会清除打断标记
 			System.out.println(Thread.currentThread().isInterrupted());
-			// 判断线程是否是中断状态,第一次调会返回当前线程的中断标志,但会将目标中断标志设置为false
+			// 判断线程是否是中断状态,返回当前线程的中断标志,但会将目标中断标志设置为false
 			System.out.println(Thread.interrupted());
 			try {
 				TimeUnit.SECONDS.sleep(1);
-				// 将线程的中断标志设置为true,即当线程被中断时,不中断,继续运行,即调用wait()时不等待,直接打断,继续运行线程
-				// 强行启动会报InterruptedException异常
+				// 如果被打断线程正在sleep(),wait(),join(),被打断线程会抛出InterruptedException,并清除打断标记(false),线程继续执行;
+				// 如果被打断线程正在运行,则会设置打断标记为true,但线程仍继续运行;
+				// 当线程A调用了LockSupport.park()之后,线程A会暂停;在线程B调用A.interrupt()之后,线程A打断标记为true;
+				// 此时LockSupoort.park()检测到打断标记为true,将不阻塞线程A;如果后续再次调用LockSupport.park(),不会有任何效果
 				Thread.currentThread().interrupt();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
